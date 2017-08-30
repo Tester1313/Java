550e6703413537ab1dbdbba7ff21275ad92c4b74
Tester1313 <thiagohs1996@gmail.com>
2017-08-30 16:44:43 -0300

    Codigo Adicionado

--- Aulas Loiane/src/aula37/Teste.java
+++ Aulas Loiane/src/aula37/Teste.java
@@ -4,10 +4,16 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
-		Aluno aluno = new Aluno();
+		/*Aluno aluno = new Aluno();
 		aluno.setNome("Thiago");
 		System.out.println(aluno.getNome());
+		aluno.setTelefone("3255555");*/
 
+		Pessoa aluno = new Aluno();
+		//aluno.
+		
+		Pessoa professor = new Professor();
+		
 	}
 
 }
