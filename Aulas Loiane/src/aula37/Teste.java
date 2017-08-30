1c96f64113589b4604569f2e697d42e6d85add33
Tester1313 <thiagohs1996@gmail.com>
2017-08-30 16:35:48 -0300

    Novas aulas adicionadas

---
+++ Aulas Loiane/src/aula37/Teste.java
@@ -0,0 +1,13 @@
+package aula37;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Aluno aluno = new Aluno();
+		aluno.setNome("Thiago");
+		System.out.println(aluno.getNome());
+
+	}
+
+}
