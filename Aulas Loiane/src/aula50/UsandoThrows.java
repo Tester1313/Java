ec048651c759c99d352b23af24e3d11517c206c5
Tester1313 <thiagohs1996@gmail.com>
2017-09-04 15:20:25 -0300

    Exceptions: StackTrace e Throws

---
+++ Aulas Loiane/src/aula50/UsandoThrows.java
@@ -0,0 +1,26 @@
+package aula50;
+
+import java.util.Scanner;
+
+public class UsandoThrows {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Entre com um número decimal: ");
+		try {
+			double num = leNumero();
+			System.out.println("Você digitou "+num);
+		} catch (Exception e) {
+			System.out.println("Entra inválida");
+			e.printStackTrace();
+		}
+	}
+	
+	// Throws irá tercerizar essa Exception
+	public static double leNumero() throws Exception {
+		Scanner scan = new Scanner(System.in);
+		double num = scan.nextDouble();
+		return num;
+	}
+
+}
