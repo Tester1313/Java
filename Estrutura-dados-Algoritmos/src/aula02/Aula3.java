ed4417489ab33bc54e639ac6509f9f2e796eb9d0
Tester1313 <thiagohs1996@gmail.com>
2017-09-09 11:22:03 -0300

    Aula 3 - Vetores e Arrays Adicionar elemento no final do vetor

---
+++ Estrutura-dados-Algoritmos/src/aula02/Aula3.java
@@ -0,0 +1,18 @@
+package aula02;
+
+public class Aula3 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(5);
+
+		if(vetor.adiciona("elemento 1")) {
+			System.out.println("Elemento adicionado");
+		}
+		vetor.adiciona("elemento 2");
+
+
+
+	}
+
+}
