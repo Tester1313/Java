e870553efdc2d43bb21dc4681353ec41f54d782d
Tester1313 <thiagohs1996@gmail.com>
2017-11-04 21:33:01 -0200

    Adiciona metodo de ordenação do List de ArrayList

--- Algoritmo Genetico/src/Matriz.java
+++ Algoritmo Genetico/src/Matriz.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
-public class Matriz {
+public class Matriz  {
 
 	private int[][] matriz;
 	private int[][] aux_matriz;
+	List list = new ArrayList<ArrayList>();
 	
 	public int[][] getaux_matriz() {
 		return aux_matriz;
@@ -34,12 +37,12 @@ public class Matriz {
 		}
 	}
 	
-	public void calcularDistancia(List listaGenes) {
+	public List<ArrayList> calcularDistancia(List listaGenes) {
 		List list = new ArrayList<ArrayList>();
 		list = listaGenes;
 		
 		
-		System.out.println("MInha lista de Genes "+list);
+		System.out.println("Minha lista de Genes    "+list);
 		//Passa em todos os ArrayList da minha Lista de ArrayList
 		for(int i = 0;i< list.size();i++) {
 			ArrayList aux = new ArrayList();
@@ -56,11 +59,37 @@ public class Matriz {
 			aux.add(custo);
 			list.remove(i);
 			list.add(i, aux);
+			this.setList(list);
 		}
+		
+		return list;
 	}
 	
 	// Busca distancia na matriz dde distancia de acordo com os parametros passados
 	private int retornaDistancia(int aux , int aux2) {
 		return this.matriz[aux][aux2];
 	}
+	
+	public void OrdenaVetor(List list) {
+		
+		//Ordena List de Arrays
+		Collections.sort (list, new Comparator() {  
+            public int compare(Object o1, Object o2) {  
+                ArrayList c1 = (ArrayList) o1;  
+                ArrayList c2 = (ArrayList) o2;
+                return (int)c1.get(10) < (int)c2.get(10) ? -1 : ((int)c1.get(10) > (int)c2.get(10) ? +1 : 0);
+              }});
+	}
+
+	public List getList() {
+		return list;
+	}
+
+	public void setList(List list) {
+		this.list = list;
+	}
+
+	
+	
 }
+
--- Algoritmo Genetico/src/main.java
+++ Algoritmo Genetico/src/main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -36,8 +38,14 @@ public class main {
 		list = geracoes.gerarGeracoes();
 		//geracoes.imprimirGeracoes();
 		
-		matriz.calcularDistancia(list);
-		geracoes.imprimirGeracoes();
+		list = matriz.calcularDistancia(list);
+		matriz.OrdenaVetor(list);
+		
+		geracoes.imprimirGeracoes(list);
+		
+	
+		
+		System.out.println(list);
 	
 	}
 
