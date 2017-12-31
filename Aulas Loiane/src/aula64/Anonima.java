e46b4fcc64826adad970102ad231002edbc5cf74
Tester1313 <thiagohs1996@gmail.com>
2017-12-31 19:04:50 -0200

    Classes aninhadas internas, locais e anonimas

---
+++ Aulas Loiane/src/aula64/Anonima.java
@@ -0,0 +1,30 @@
+package aula64;
+
+public class Anonima {
+	
+	public void imprimeTexto() {
+		System.out.println("Qualquer texto.");
+	}
+
+	public static void main(String[] args) {
+		
+		Anonima anonima = new Anonima() {
+			public void imprimeTexto() {
+				System.out.println("Qualquer texto que foi sobrescrito");
+			}
+		};
+
+		anonima.imprimeTexto();
+		
+		//Usando Interface
+		Texto texto = new Texto() {
+			@Override
+			public void imprimeTexto() {
+				System.out.println("qualquer texto - interface");
+			}
+		};
+		
+		texto.imprimeTexto();
+	}
+
+}
